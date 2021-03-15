ed528096027140389ae8fda9cfff898779f11646
Marlon Costa <marlon.costa@academico.ifpb.edu.br>
2021-03-14 23:04:12 -0300

    Adição de log
    Logs adicionados com conteúdo na saída padrão

--- Python/main.py
+++ Python/main.py
@@ -1,8 +1,16 @@
+import sys
+
 from zeep import Client
+import logging
 
-client = Client("http://www.learnwebservices.com/services/hello?WSDL")
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
-request = {"Name": "Naressi"}
+if __name__ == '__main__':
+    client = Client("http://www.learnwebservices.com/services/hello?WSDL")
 
-result = client.service.SayHello(request)
-print(result)
+    request = {"Name": "Naressi"}
+    logging.info(f"Request body: {request}")
+    result = client.service.SayHello(request)
+    logging.info(f"Result: {result}")
+    print(result)
+    print("WITORIA TCHOP")
